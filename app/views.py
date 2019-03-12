9d889c36da38edbf34662e822d80f8cd8d7f0f05
蔡目浩 <caimuhao@gmail.com>
2019-03-12 21:31:24 +0800

    图片上传

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,jsonify
 from app import app
 from app import photos
 import os
+from datetime import datetime
+
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -28,4 +30,17 @@ def delete_file(filename):
     file_path = photos.path(filename)
     os.remove(file_path)
     return redirect(url_for('manage_file'))
+
+@app.route('/photoListJson',methods=['GET'])
+def photo_list_json():
+    photo_list = os.listdir(app.config['UPLOADED_PHOTOS_DEST'])
+    phone_name = []
+    for photo in photo_list:
+        timeFormat = photo.split('.')[0]
+        time = datetime.strptime(timeFormat,"%Y-%m-%d-%H-%M-%S")
+        phone_name.append("%s年%s月%s日%s时%s分%s秒 星期%s"%(time.year,time.month,time.day,time.hour,time.minute,time.second,time.isoweekday()))
+    photo_list=photo_list+photo_list
+    phone_name=phone_name+phone_name
+    return jsonify({"name":phone_name,"photo":photo_list})
+
     
